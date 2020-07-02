ae0207f0b69d9debb82ae11193bedd546aa82f01
josh <joshua_bousfield@yahoo.com>
2020-07-02 19:07:16 +1000

    a grid is now created based on number of squares needed

--- src/com/joshbousfield/tictacktoe/GameSquare.java
+++ src/com/joshbousfield/tictacktoe/GameSquare.java
@@ -6,23 +6,26 @@ package com.joshbousfield.tictacktoe;
 import java.util.HashMap;
 import java.util.Map;
 import com.joshbousfield.tictacktoe.PlayPiece.Type;
+import javafx.scene.image.Image;
 
 public class GameSquare {
     private final int xPos;
     private final int yPos;
+    private final CoOrd coOrd;
     private boolean occupied;
+    private Image tile;
     private Type type;
     private Map<String, GameSquare> matchingSquares;
 
-    public GameSquare(int xPos, int yPos, Type type, Map<String, GameSquare> matchingSquares) {
+    public GameSquare(int xPos, int yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
+        this.coOrd = new CoOrd(xPos, yPos);
         occupied = false;
+        this.tile = new Image(getClass().getResource("images/gamesquare.png").toExternalForm());//needs testing
         this.type = Type.NONE;
         this.matchingSquares = new HashMap<>();
     }
-
-
 
     public int getxPos() {
         return xPos;
@@ -53,11 +56,46 @@ public class GameSquare {
         }
     }
 
+    public Image getTile() {
+        return tile;
+    }
+
+    public void setTile(Image tile) {
+        this.tile = tile;
+    }
+
     public Map<String, GameSquare> getMatchingSquares() {
         return matchingSquares;
     }
 
     public void setMatchingSquares(Map<String, GameSquare> matchingSquares) {
         this.matchingSquares = matchingSquares;
+    }
+
+    public CoOrd getCoOrd() {
+        return coOrd;
+    }
+
+    public class CoOrd {
+        int x;
+        int y;
+
+        public CoOrd(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(getX()) + getY();
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
     }
 }
--- src/com/joshbousfield/tictacktoe/MainController.java
+++ src/com/joshbousfield/tictacktoe/MainController.java
@@ -1,8 +1,37 @@
 package com.joshbousfield.tictacktoe;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController {
     @FXML public GridPane gameSquares;
+    private final Map<GameSquare.CoOrd, GameSquare> squares = new HashMap<>();
+
+
+    public void initialize() {
+        setBoard(3, 3);
+        setGrid();
+    }
+
+    private void setBoard(int xRow, int yRow) {
+        GridPane gridPane = new GridPane();
+        for(int x=0; x<xRow; x++) {
+            for(int y=0; y<yRow; y++) {
+                GameSquare square = new GameSquare(x, y);
+                System.out.println(square.getCoOrd().toString());
+                squares.put(square.getCoOrd(), square);
+            }
+        }
+    }
+
+    private void setGrid() {
+        for (Map.Entry<GameSquare.CoOrd, GameSquare> square : squares.entrySet()) {
+            ImageView view = new ImageView();
+            view.setImage(square.getValue().getTile());
+            gameSquares.add(view, square.getKey().getX(), square.getKey().getY());
+        }
+    }
 }
